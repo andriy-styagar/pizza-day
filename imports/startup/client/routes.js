0078f81b3779f904cce66d83182e7f93468dd6bc
Andriy Styagar <standriy44@gmail.com>
2017-03-14 19:29:40 +0200

    Create app structure

---
+++ imports/startup/client/routes.js
@@ -0,0 +1,8 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+// Import needed templates
+import '../../ui/layouts/body/body.js';
+
+
+// Set up all routes in the app
