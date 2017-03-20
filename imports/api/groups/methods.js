9024ce6f9aad457a131da6e67640ff2648523eb7
Andriy Styagar <standriy44@gmail.com>
2017-03-21 00:43:01 +0200

    added add/remove participants and remove groups options

--- imports/api/groups/methods.js
+++ imports/api/groups/methods.js
@@ -23,5 +23,35 @@ Meteor.methods({
 				});			
 			};
 		});
-	}
+	},
+	'removeGroup'(groupId){
+		check(groupId, String);
+		const group = Groups.find({_id: groupId}).fetch()[0];
+		const arrId = group.participants;
+		arrId.forEach(function(id){
+			Meteor.users.update(
+				{_id: id},
+				{$set: {'profile.group': null}});
+		});
+		Meteor.users.update(
+			{_id: group.admin},
+			{$set: {'profile.group': null, 'profile.groupAdmin': false}});
+		Groups.remove({_id: groupId});
+	},
+	'addParticapant'(userId,groupId){
+		check(userId, String);
+		check(groupId, String);
+		Groups.update({_id: groupId},
+			{ $push: {participants: userId }});
+		Meteor.users.update({ _id: userId },
+			{ $set: {'profile.group': groupId }});
+		},
+	'removeParticipant'(userId,groupId){
+		check(userId, String);
+		check(groupId, String);
+		Groups.update({_id: groupId},
+			{$pull: {participants: userId}});
+		Meteor.users.update({ _id: userId },
+			{ $set: {'profile.group': null }});
+		}
 });
--- imports/ui/components/groups/group-item/group-item.js
+++ imports/ui/components/groups/group-item/group-item.js
@@ -1,13 +1,16 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Images, Groups } from '../../../../api/groups/collections.js';
+import { throwError } from '../../../../api/errors/error.js';
+
 import './group-item.html';
 
 Template.groupItem.onCreated(function () {
 	const groupId = FlowRouter.getParam('groupId');
-	this.subscribe('groupItem', groupId, function(){
+	Meteor.subscribe('groupItem', groupId, function(){
 			const group = Groups.find({_id: FlowRouter.getParam('groupId')}).fetch()[0];
 			Meteor.subscribe('images', group.logo);
 	});
+	Meteor.subscribe('users');
 })
 Template.groupItem.helpers({
 	'groupData'(){
@@ -15,10 +18,60 @@ Template.groupItem.helpers({
 		const data = {};
 		data.name = group.name;
 		data.logoUrl = Images.find({_id: group.logo}).fetch()[0].url();
+		data.groupParticipants = Meteor.users.find(
+				{ $and: [
+					{'profile.group': FlowRouter.getParam('groupId')},
+					{ $or: [
+						{'profile.groupAdmin': false },
+						{"profile.groupAdmin": { $exists: false}}] 
+					}
+				]},
+				{ fields: {_id: 1, profile: 1 }});
+		data.admin = Meteor.users.find({_id: group.admin}).fetch()[0];
 		return data
 	},
-	'idGrouppAdmin'(){
+	'addParticipantsList'(){
+		const users = Meteor.users.find({ $or: [
+			{"profile.group": null},
+			{"profile.group": {$exists: false}},
+			{ fields: {_id: 1, profile: 1 }}
+		]});
+		return users;
+	},
+	'isGrouppAdmin'(){
 		const profile = Meteor.user().profile;
 		return profile.group == FlowRouter.getParam('groupId') && profile.groupAdmin;
+	},
+
+});
+
+Template.groupItem.events({
+	'click .add-participant-but': function(e, t){
+		const userId = $(e.target).attr('id');
+		const groupId = FlowRouter.getParam('groupId');
+		Meteor.call('addParticapant', userId, groupId, function (err) {
+			if(err){
+				throwError(err.reason);
+			}
+		})
+	},
+	'click .remove-participant-but': function(e, t){
+		const userId = $(e.target).attr('id');
+		const groupId = FlowRouter.getParam('groupId');
+		Meteor.call('removeParticipant', userId, groupId, function (err) {
+			if(err){
+				throwError(err.reason);
+			}
+		})
+	},
+	'click #remove-group-but': function(e, t){
+		const groupId = FlowRouter.getParam('groupId');
+		Meteor.call('removeGroup', groupId, function (err) {
+			if(err){
+				throwError(err.reason);
+			}
+			else
+				FlowRouter.go('/groups');
+		})
 	}
 })
